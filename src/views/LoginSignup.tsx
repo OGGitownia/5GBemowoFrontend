8f0309cef682d1c34d1f51d5d8b08d70d8422e8c
Mikolaj <mkolodzi>
2025-05-12 22:21:52 +0200

    failed connection with backend

--- src/views/LoginSignup.tsx
+++ src/views/LoginSignup.tsx
@@ -9,9 +9,37 @@ import password_icon from "../assets/password.png";
 type Props = {
     onLoginSuccess: () => void;
 };
+const handleClick = async (action: string, username: string, email: string, password: string, setAction: (val: string) => void) => {
+    if (action === "Sign Up") {
+        const url = "http://localhost:8080/api/users/register/email";
+        const data = {username: username, email: email, password: password};
+
+        try {
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Accept": "application/json",
+                },
+                body: JSON.stringify(data),
+            });
+
+            const result = await response.json();
+            console.log(data);
+            console.log(result);
+        } catch (error) {
+            console.error("Błąd podczas wysyłania żądania:", error);
+        }
+    } else {
+        setAction("Sign Up");
+    }
+};
+
 
 function LoginSignup({ onLoginSuccess }: Props) {
     const [action, setAction] = useState("Sign Up");
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
     const handleLoginClick = () => {
         // Tutaj normalnie dodałbyś fetch do backendu i walidację.
@@ -29,17 +57,33 @@ function LoginSignup({ onLoginSuccess }: Props) {
                 {action === "Login" ? null : (
                     <div className="input">
                         <img src={user_icon} alt="user icon" />
-                        <input type="text" placeholder="Name" />
+                        <input
+                            type="text"
+                            placeholder="Name"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
                     </div>
                 )}
                 <div className="input">
                     <img src={email_icon} alt="email icon" />
-                    <input type="email" placeholder="Email" />
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
                 </div>
                 <div className="input">
                     <img src={password_icon} alt="password icon" />
-                    <input type="password" placeholder="Password" />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                 </div>
+
             </div>
 
             {action === "Sign Up" ? null : (
@@ -51,7 +95,7 @@ function LoginSignup({ onLoginSuccess }: Props) {
             <div className="submit-container">
                 <div
                     className={action === "Login" ? "submit gray" : "submit"}
-                    onClick={() => setAction("Sign Up")}
+                    onClick={() => handleClick(action, username, email, password, setAction)}
                 >
                     Sign Up
                 </div>
