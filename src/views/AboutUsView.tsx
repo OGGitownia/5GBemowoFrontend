eef1cc6d0a17775a3f59b1ffcedc0c020bfb9745
Desktop <krykubi@wp.pl>
2025-05-27 00:15:14 +0200

    kommiit na uspoujnienie wersji po nie udanym merge2

--- src/views/AboutUsView.tsx
+++ src/views/AboutUsView.tsx
@@ -1,14 +1,16 @@
 import "../styles/AboutUsView.css";
 import BackButton from "../components/smallerComponents/BackButton.tsx";
+import RightIcons from "../components/smallerComponents/RightIcons.tsx";
 
 const AboutUsView = () => {
-  return (
-      <div className="about-us-container">
-        <div className="top-bar">
-          <BackButton />
+    return (
+        <div className="about-us-container">
+            <div className="top-bar">
+                <BackButton />
+                <RightIcons />
+            </div>
         </div>
-      </div>
-  );
+    );
 };
 
 export default AboutUsView;
